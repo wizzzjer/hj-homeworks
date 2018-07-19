701ff852597df70b02f8a677d1b4f06bf0dba438
Svetlana Korobtseva <wizzzjer@gmail.com>
2018-07-19 17:44:27 +0800

    Bubbling HW aaded

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function handleItem(event) {
+  event.preventDefault(); // чтобы страница не прыгала наверх, когда кликаешь на один из последних товаров
+  if (event.target.classList.contains('add-to-cart')) {
+    let addThisItem = {
+      title: event.target.dataset.title,
+      price: event.target.dataset.price
+    };
+    addToCart(addThisItem);
+  }
+}
+
+let itemsList = document.getElementsByClassName('items-list')[0];
+itemsList.addEventListener('click', handleItem);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.classList.contains('prop__name')) {
+    if (!event.target.dataset.dir || event.target.dataset.dir == '-1') {
+      event.target.dataset.dir = '1';
+    } else {
+      event.target.dataset.dir = '-1';
+    }
+    sortTable(event.target.dataset.propName, event.target.dataset.dir);
+    event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+  }
 }
